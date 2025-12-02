e43371999d276db648c80dab88655e3ec661c744
rajubanda81@gmail.com <rajubanda81@gmail.com>
2025-12-02 13:01:27 +0530

    first Commit

---
+++ index.php
@@ -0,0 +1,101 @@
+ <?php
+
+          // class Car
+          // {
+          //           public $name, $color, $model;
+
+          //           public function __construct($name, $color, $model)
+          //           {
+          //                     $this->name = $name;
+          //                     $this->color = $color;
+          //                     $this->model = $model;
+          //           }
+
+          //           public function message()
+          //           {
+          //                     return "My car is a " . $this->color . " " . $this->name . " " . $this->model . "!";
+          //           }
+          // }
+
+          // $newCar = new Car("Toyota", "black", "2020");
+          // $newCar1 = new Car("Suzuki", "Red", "2022");
+          // echo $newCar->message();
+          // echo "<br/>";
+          // echo $newCar1->message(); 
+
+          // function myFunction($name = "Raju")
+          // {
+          //           echo "Hello World!" . $name;
+          // }
+
+          // myFunction("John");
+          // myFunction();
+          // myFunction("David");
+          // 
+
+
+          // $arr = ["apple", "bat", "cat", "dog", "elephnat"];
+          // $result = implode(' ', $arr);
+
+          // echo $result;
+
+          // $str = "Hello World Welcome to New World";
+          // $result = strlen($str);
+          // $result = str_word_count($str);
+          // $result = strrev($str);
+          // $result = strpos($str, "New");
+          // $result = str_replace("World", "Universe", $str);
+
+          // echo $result;
+          // $result = explode(" ", $str);
+          // print_r($result);
+
+
+          // $arr_1 = [1, 2, 3, 4, 5];
+          // $arr_2 = [6, 7, 8, 9, 10];
+          // $result = array_merge($arr_1, $arr_2);
+          // $result = array_combine($arr_1, $arr_2);
+          // print_r($result);
+
+
+
+          //define associative array 2 arrays and combine them-->
+          // $arr_1 = ["a" => "apple", "b" => "bat", "c" => "cat"];
+          // $arr_2 = [1, 2, 3];
+          // $result = array_combine($arr_1, $arr_2);
+          // print_r($result);
+
+          // $arr = ['apple', 'banana', 'grape', 'mango'];
+          // // echo $arr[1];
+          // foreach ($arr as $a) {
+          //           echo $a;
+          //           echo "<br/>";
+          // }
+
+
+          // $fruits = ["apple", "banana", "cherries", "dates", "elderberry"];
+          // for ($i = 0; $i < count($fruits); $i++) {
+          //           echo $fruits[$i];
+          //           echo "<br/>";
+          // }
+
+
+
+          // $employees = [
+          //           ["name" => "John", "age" => 30, "position" => "Manager"],
+          //           ["name" => "Jane", "age" => 25, "position" => "Developer"],
+          //           ["name" => "Doe", "age" => 28, "position" => "Designer"]
+          // ];
+
+          // foreach ($employees as $employee) {
+          //           echo "Name: " . $employee['name'] . ", Age: " . $employee['age'] . ", Position: " . $employee['position'];
+          //           echo "<br/>";
+          // }
+
+
+          //define associate array with print key and value-->
+          // $person = ['name' => 'John', 'age' => 30, 'city' => 'New York'];
+          // foreach ($person as $key => $value) {
+          //           echo $key . ": " . $value;
+          //           echo "<br/>";
+          // }
